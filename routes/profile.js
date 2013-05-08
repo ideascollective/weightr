0d12120d87acfd466ee03de6cbc83bde5eb9188d
Franco Monsalvo <franco.monsalvo@gmail.com>
2013-05-08 01:04:34 -0300

    adding and updating routes

---
+++ routes/profile.js
@@ -0,0 +1,7 @@
+
+/*
+ * GET a user's Profile by ID
+ */
+exports.get = function(req, res){
+  res.send("respond with a resource");
+};
---
+++ routes/weight.js
@@ -0,0 +1,21 @@
+
+/*
+ * GET a user's weight page
+ */
+exports.get = function(req, res){
+  res.send("respond with a resource");
+};
+
+/*
+ * POST a weight record for the given user
+ */
+exports.post = function(req, res){
+  res.send("respond with a resource");
+};
+
+/*
+ * GET a user's weight records list
+ */
+exports.list = function(req, res){
+  res.send("respond with a resource");
+};
